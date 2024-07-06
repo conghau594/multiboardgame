1a3caaca1d6aabeb53b71840232b089ba4ab5717
Cong Hau <conghau594@gmail.com>
2024-07-06 18:35:17 +0700

    add content to the main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,26 @@
-#include <iostream>
+import core.Client;
+import app.SDLAppViewFrame;
 
-int main() {
-  return 1;
+#ifdef _DEBUG
+#include <catch2/catch_session.hpp>
+
+int main(int argc, char* args[])
+
+#else  //!_DEBUG
+#include <Windows.h>
+
+int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow)
+#endif //_DEBUG
+
+{
+#ifdef _DEBUG
+	//run test cases
+	Catch::Session().run();
+#endif
+
+	//main definition
+	mbg::Client client1(mbg::SDLAppViewFrame::createPointer(60));
+	client1.run(0);
+
+	return 0;
 }
